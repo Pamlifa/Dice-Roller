120e032703ef41652a18ea18d485e934de9e8a5a
Catz GaGa <catzgaga@gmail.com>
2024-10-28 15:37:02 +0100

    Initial commit

---
+++ diceroller.js
@@ -0,0 +1,20 @@
+//DICE ROLLAR PROGRAM //
+
+function rollDice() {
+
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for (let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1
+        //console.log(value);
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.png">`);
+    }
+    // console.log(values);
+    diceResult.textContent = `Dice: ${values.join(',')}`;
+    diceImages.innerHTML = images.join('');
+}
